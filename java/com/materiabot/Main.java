b7b54007c9dbd27b3d4e4b41a70c4a0db0e7ed11
MFaisca <MFaisca@HEAVEN.home>
2020-02-17 00:44:53 +0000

    Improved Connectivity Management

--- java/com/materiabot/Main.java
+++ java/com/materiabot/Main.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
+		PluginManager.loadCommands();
 		String privateToken = SQLAccess.getKeyValue(SQLAccess.BOT_TOKEN_KEY);
 		if(privateToken == null) {
 			System.out.println("Bot Token isn't inserted." + System.lineSeparator());
@@ -16,11 +17,10 @@ public class Main {
 		}
         JDA client = new JDABuilder(privateToken).setAutoReconnect(true)
 				.setStatus(OnlineStatus.ONLINE)
-				.setActivity(Activity.playing("Opera Omnia")).addEventListeners(new _Listener()).build();
+				.setActivity(Activity.playing("Opera Omnia")).build();
 		client.awaitReady();
-		_Listener.setClient(client);
-		Constants.getClient();
-		PluginManager.loadCommands();
+		client.addEventListener(new _Listener());
+		Constants.setClient(client);
 		//PluginManager.loadUnits();
 		System.out.println("Bot is ready!!");
 	}
