6f1cbdbad998ff2938b659615b9c4427d6774aff
mfais <mfais@HEAVEN.home>
2020-05-22 00:43:28 +0100

    Added Gateways

--- java/com/materiabot/Main.java
+++ java/com/materiabot/Main.java
@@ -2,11 +2,15 @@ package com.materiabot;
 import com.materiabot.IO.SQL.SQLAccess;
 import com.materiabot.Utils.Constants;
 import com.materiabot.commands._Listener;
+import java.util.Arrays;
+import java.util.List;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -16,9 +20,12 @@ public class Main {
 			return;
 		} 
 		System.out.println("Connected to DB");
+		final List<GatewayIntent> gateways = Arrays.asList(	GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MEMBERS, 
+															GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_MESSAGES, 
+															GatewayIntent.GUILD_EMOJIS);
         JDA client = JDABuilder.createDefault(privateToken).setAutoReconnect(true)
-				.setStatus(OnlineStatus.ONLINE).setMemberCachePolicy(MemberCachePolicy.NONE)
-				.setActivity(Activity.playing("Opera Omnia")).build();
+				.setEnabledIntents(gateways).setMemberCachePolicy(MemberCachePolicy.NONE).disableCache(CacheFlag.VOICE_STATE)
+				.setStatus(OnlineStatus.ONLINE).setActivity(Activity.playing("Opera Omnia")).build();
 		Constants.setClient(client);
 		PluginManager.loadCommands();
 		PluginManager.loadUnits();
