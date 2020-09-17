2803a47eabd5594583b0e788fe5e8d1568eea4e8
mfais <mfais@192.168.1.7>
2020-09-17 23:11:40 +0100

    .

--- java/com/materiabot/PluginManager.java
+++ java/com/materiabot/PluginManager.java
@@ -5,7 +5,7 @@ import org.plugface.core.PluginRef;
 import org.plugface.core.factory.PluginManagers;
 import org.plugface.core.factory.PluginSources;
 import com.materiabot.IO.JSON.UnitParser;
-import com.materiabot.IO.JSON.UnitParser.OverrideManager;
+//import com.materiabot.IO.JSON.UnitParser.OverrideManager;
 import com.materiabot.Utils.Constants;
 import com.materiabot.commands._BaseCommand;
 import com.materiabot.commands._Listener;
@@ -27,22 +27,22 @@ public class PluginManager {
 		Constants.COMMANDS.sort((c1, c2) -> c1.getCommand().compareTo(c2.getCommand()));
 	}
 
-	@SuppressWarnings("rawtypes")
-	public static void loadManagers() throws Exception {
-		org.plugface.core.PluginManager manager = PluginManagers.defaultPluginManager();
-		for(PluginRef p : manager.getAllPlugins().stream().filter(p -> p.getName().contains("Override.")).collect(Collectors.toList()))
-			manager.removePlugin(p.getName());
-		manager.loadPlugins(PluginSources.jarSource("file:///" + new java.io.File("plugins").getAbsolutePath().replace("\\", "/")));
-		System.out.println("---");
-		manager.getAllPlugins().stream()
-			.filter(p -> p.getName().contains("Override."))
-			.forEach(p -> System.out.println(p.getName()));
-		UnitParser.overrideManagerCollection.clear();
-		manager.getAllPlugins().stream()
-			.filter(p -> p.getName().contains("Override."))
-			.map(p -> (OverrideManager)p.get())
-			.forEach(c -> UnitParser.overrideManagerCollection.add(c));
-	}
+//	@SuppressWarnings("rawtypes")
+//	public static void loadManagers() throws Exception {
+//		org.plugface.core.PluginManager manager = PluginManagers.defaultPluginManager();
+//		for(PluginRef p : manager.getAllPlugins().stream().filter(p -> p.getName().contains("Override.")).collect(Collectors.toList()))
+//			manager.removePlugin(p.getName());
+//		manager.loadPlugins(PluginSources.jarSource("file:///" + new java.io.File("plugins").getAbsolutePath().replace("\\", "/")));
+//		System.out.println("---");
+//		manager.getAllPlugins().stream()
+//			.filter(p -> p.getName().contains("Override."))
+//			.forEach(p -> System.out.println(p.getName()));
+//		UnitParser.overrideManagerCollection.clear();
+//		manager.getAllPlugins().stream()
+//			.filter(p -> p.getName().contains("Override."))
+//			.map(p -> (OverrideManager)p.get())
+//			.forEach(c -> UnitParser.overrideManagerCollection.add(c));
+//	}
 
 	@SuppressWarnings("rawtypes")
 	public static void loadUnits() throws Exception {
